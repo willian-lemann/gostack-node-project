980140b2975ab1e4a13ba76bc929c5bde71fc626
willian-lemann <willianleman@hotmail.com>
2021-01-04 00:18:26 -0300

    model users created

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', async (request, response) => {
+   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+   const appointments = await appointmentsRepository.find();
+   return response.json(appointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+   try {
+      const { provider, date } = request.body;
+
+      const parsedDate = parseISO(date);
+
+      const createAppointmentService = new CreateAppointmentService();
+
+      const appointment = await createAppointmentService.Execute({
+         provider,
+         date: parsedDate,
+      });
+
+      return response.json(appointment);
+   } catch (err) {
+      return response.status(400).json({ error: err.message });
+   }
+});
+
+export default appointmentsRouter;
